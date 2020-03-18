c9ec820eb8253d869be4961e181e159393813c87
Andreas Börzel <andreas.boerzel@gmx.de>
2020-03-18 15:50:44 +0100

    refactor: unused config variables removed

--- config/license_recognition/config.py
+++ config/license_recognition/config.py
@@ -1,33 +1,7 @@
-# import the necessary packages
-import os
-
-# root paths
-DATASET_ROOT_PATH = "data"
-
 # network image size
 IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 64
 
-# training parameter
-BATCH_SIZE = 64
-NUM_EPOCHS = 1000
-
 # license number construction
 DOWNSAMPLE_FACTOR = 2 ** 2  # <= pool size ** number of pool layers
 MAX_TEXT_LEN = 10
-
-# model name
-MODEL_NAME = "glpr-model"
-
-# supported optimizer methods: sdg, rmsprop, adam, adagrad, adadelta
-OPTIMIZER = "adagrad"
-
-# define the path to the output directory used for storing plots, classification reports, etc.
-OUTPUT_PATH = "output"
-DOCUMENTATION_PATH = "documentation"
-
-# define the paths to the training and validation directories
-TEST_IMAGES = "testimages"
-
-# define the path to the output training, validation, and testing HDF5 files
-GLP_HDF5 = os.path.join(DATASET_ROOT_PATH, "glp.h5")
